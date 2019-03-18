e6c077ac2267d5d522f583c0786af55bd2b19248
georgechifan <george.chifan@yahoo.com>
2019-03-18 23:10:37 +0200

    JavaScript
    saptamana 5
    curs 1_in sala

---
+++ saptamana 5/index.js
@@ -0,0 +1,10 @@
+var initialstring="hello world";
+console.log(initialstring);
+
+
+document.getElementById("demo").innerHTML="my first javascript";
+
+
+function myFunction() {
+    alert("Hlo World!");
+  }
