9583ee9afd8c5511043c2ebe53678b5e317b669e
georgechifan <george.chifan@yahoo.com>
2019-03-31 23:48:58 +0300

    actualizare tema pentru luni_javaScript

---
+++ saptamana 5/index2.js
@@ -0,0 +1,136 @@
+// // function greaterNumber(x,y){
+// //     if(x===y){
+// //         return "numerele sunt egale";
+// //     }
+// //     else {
+// //     if(x>y) {
+// //        return "numarul mai mare dintre " + x + " si " + y + " este " + x;
+// //     } else {
+// //        return "numarul mai mare dintre " + x + " si " + y + " este " + y;
+// //     } 
+// //     }
+// // }
+// // console.log(greaterNumber(0,7));
+.....................................................
+
+// // var helloWorldInLang=function(code){
+// //     var languagesForHw={
+// //         "en": "hello",
+// //         "es": "holla",
+// //         "ro": "buna"
+// //     }
+// //     return languagesForHw[code];
+// // }
+// // console.log(helloWorldInLang("en"));
+......................................................
+
+// function fnName1(){
+//     console.log("hello");
+//     break;
+// }
+//     function fnName2(){
+//     console.log("olla");
+//     break;
+// }
+//         function fnName3(){
+//         console.log("buna");
+//         break;
+// }
+
+// var helloWorldInLang=function(code){
+//     var languagesForHw={
+//         "en": fnName1(),
+//         "es": fnName2(),
+//         "ro": fnName3(),
+//     }
+// }
+// console.log(helloWorldInLang("es"))
+.....................................................
+
+// var helloWorldInLang= function(code){
+//     switch(code){
+//         case "en": return "Hello World";
+//         break;
+    
+//         case "de": return "Gooten tag";
+//         break;
+
+//         case "es": return "Holla muchachos";
+//         break;
+
+//         default: return "buna";
+//     }
+// }
+// console.log(helloWorldInLang("de"));
+// //---WORKING
+
+
+
+// // // // // function pluralize(animal, number){
+// // // // //         if(number===1){
+// // // // //             return "1" + subst;
+// // // // //         } else {
+// // // // //             return nr + subst + "s";
+// // // // //         }
+// // // // //     }
+// // // // // pluralize("sheep");
+
+
+
+// // // function computeTaxes(venitAnual){
+// // //     switch (true){
+// // //         case venitAnual<30000:
+// // //         return 0;
+// // //         breack;
+// // // case venitAnual>50000&&
+// // //      venitAnual<99999:
+// // //      return 0.35*venitAnual;
+// // //      breack;
+// // // case venitAnual>100000:
+// // //      return 0.4*venitAnual;
+// // //      breack;
+// // //     }
+// // // }
+
+// // // console.log(computeTaxes(80000));
+
+
+// // // // var isDateInWeekend=function(date){
+// // // //     var parsedDate=new Date(date);
+// // // //     var indexOfDay=parsedDate.getDay();
+// // // //   return [5,6].includes(indexOfDay);
+// // // // }
+// // // // isDateInWeekend("Nov 14,2018");
+
+
+// // var day;
+// // switch (new Date().getDay()) {
+// //   case 0:
+// //     day = "Sunday";
+// //     break;
+// //   case 1:
+// //     day = "Monday";
+// //     break;
+// //   case 2:
+// //     day = "Tuesday";
+// //     break;
+// //   case 3:
+// //     day = "Wednesday";
+// //     break;
+// //   case 4:
+// //     day = "Thursday";
+// //     break;
+// //   case 5:
+// //     day = "Friday";
+// //     break;
+// //   case  6:
+// //     day = "Saturday";
+// // }
+
+
+// function addTwoNumbers(first,second){
+//     var result=first+second;
+//     console.log(result);
+// }
+// addTwoNumbers(2,6);
+
